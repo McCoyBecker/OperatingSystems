09ef463a61ccabc2731c4f159b44f7f5a53f232e
McCoy Becker <mccoybecker@gmail.com>
2018-06-23 22:52:04 -0500

    Included Intro files

--- cpu.c
+++ cpu.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <assert.h>
 #include "common.h"
 
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+	fprintf(stderr, "usage: cpu <string>\n");
+	exit(1);
+    }
+    char *str = argv[1];
 
-int main(int argc, char const *argv[])
-{
-    if (argc!=2){
-        fprintf(stderr,"usage: cpu <string>\n");
-        exit(1);
-        }
-
-        char *str = argv[1];
-        while (1){
-            Spin(1);
-            printf("%s\n",str);
-        }
+    while (1) {
+	printf("%s\n", str);
+	Spin(1);
+    }
     return 0;
 }
+
